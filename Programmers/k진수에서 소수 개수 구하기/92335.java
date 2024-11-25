2d68e1a8ea7cedce26ea35990f16b000f229826b
kkyeongin <kkyeongin@gmail.com>
2024-11-26 00:06:23 +0900

    문제 추가

---
+++ Programmers/k진수에서 소수 개수 구하기/92335.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+
+class Solution {
+    public int solution(int n, int k) {
+        return count(divide(n,k));
+    }
+
+    public static int count(String n){
+        return (int) Arrays.stream(n.split("0"))
+        .map(Long::parseLong)
+        .filter(Solution::isPrime)
+        .count();
+    }
+
+    public static boolean isPrime(Long n) {
+        if (n <= 1) return false;  
+        if (n <= 3) return true;          
+        if (n % 2 == 0 || n % 3 == 0) return false; 
+    
+        for (long i = 5; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) return false;
+        }
+
+        return true;
+    }
+
+    public static String divide(int n, int k){
+        if(k == 10) return Integer.toString(n);
+        
+        StringBuilder result=new StringBuilder();
+        while(n>k){
+            result.append(n%k);
+            n = n/k;
+        }
+        result.append(n);
+        return result.reverse().toString();
+    }
+}
+
